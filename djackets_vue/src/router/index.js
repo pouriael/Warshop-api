8572d432f23304dff7e9f770b7dce24338b8fb07
pouria <pouriael2002@gmail.com>
2023-02-03 20:39:56 +0330

    first

---
+++ djackets_vue/src/router/index.js
@@ -0,0 +1,99 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+import HomeView from '../views/HomeView.vue'
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+import Success from '../views/Success.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/search',
+    name:'Search',
+    component: Search
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name:'Product',
+    component:Product
+  },
+  {
+    path: '/:category_slug',
+    name:'Category',
+    component:Category
+  },
+  {
+    path: '/cart',
+    name:'Cart',
+    component: Cart
+  },
+  {
+    path: '/sign-up',
+    name:'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name:'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/my-account',
+    name:'MyAccount',
+    component: MyAccount,
+    meta:{
+      requireLogin:true
+    }
+      
+  },
+  {
+    path: '/cart/checkout',
+    name:'Checkout',
+    component: Checkout,
+  },
+  {
+    path: '/cart/success',
+    name:'Success',
+    component: Success,
+    meta:{
+      requireLogin:true
+    }
+      
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to,from,next) =>{
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated){
+    next({name:'LogIn',query: {to: to.path}});
+  } else{
+    next()
+  }
+})
+
+export default router
+
+
